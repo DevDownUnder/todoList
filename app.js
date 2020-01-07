901a75af7ade67deb68ef2e4582e7a73366bdbd1
Tim <tim.weinert@gmail.com>
2020-01-07 14:59:02 +1100

    Object created. Method to display  array created.

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
+//Create object for Todo List
 let todoList = {
-todos: ["banana", "apple", "orange", "cherry"],
+  //Create array (list) variable within object for possible items
+  todos: ["item 1", "item 2", "item 3"],
+  //Create method (function within an object) to display the list items
+  displayTodo: function () {
+    //this. refers to the object (todoList) and todos is the property
+    console.log(this.todos)
+  }
 };
 
-alert(todoList.todos);
+//Run the method (function) to display the list items in the console
+todoList.displayTodo()
