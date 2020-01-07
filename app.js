d42ce30dad9235c80e6556d0af04fe45a5612a86
Tim <tim.weinert@gmail.com>
2020-01-07 15:13:03 +1100

    Add to list method created

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 //Create object for Todo List
 let todoList = {
+  //INITIAL LIST
   //Create array (list) variable within object for possible items
   todos: ["item 1", "item 2", "item 3"],
+
+  //DISPLAY LIST
   //Create method (function within an object) to display the list items
   displayTodo: function () {
+    //this. refers to the object (todoList) and todos is the property
+    console.log(this.todos)
+  },
+
+  //ADD TO LIST
+  //Create method (function within an object) to add to the list items
+  addTodo: function (item) {
+    this.todos.push(item)
     //this. refers to the object (todoList) and todos is the property
     console.log(this.todos)
   }
 };
 
-//Run the method (function) to display the list items in the console
+//RUN METHODS (FUNCTIONS)
+
+//DISPLAY METHOD
 todoList.displayTodo()
+
+
+//ADD TO LIST METHOD
+todoList.addTodo("item 4")
